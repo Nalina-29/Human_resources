9444070525d625a97427da9bec940eb1306ea541
sys <sys@DESKTOP-V876IOA>
2023-08-17 10:17:09 +0530

    edited

--- src/test/java/practice/HrAssistant.java
+++ src/test/java/practice/HrAssistant.java
@@ -7,5 +7,14 @@ public class HrAssistant {
 public void create()
 {
 	System.out.println("created emp");
+	System.out.println("edit the emp");
+	
+}
+@Test
+public void edit()
+{
+	
+	System.out.println("edited emp");
 }
 }
+
